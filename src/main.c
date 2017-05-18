2bb45463b393bb577de7ed8e359bf99422925332
Satan Love <panmihaurkin@icloud.com>
2017-05-18 16:19:30 +0700

    Added basic reading bones

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <process.h>
+#include <strings.h>
+
+int main(int argc, char *argv[])
+{
+	if (scmp(argv[1], "lab.c") == 0) {
+		get_by_prttpe(argv[1]);
+	} else {
+		printf("Usage:\n");
+		printf("canalize <in-file-name>\n");
+	}
+
+	return 0;
+}
