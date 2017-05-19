598947c7d1285f78fdb8df43489e386db0b2db9a
Satan Love <panmihaurkin@icloud.com>
2017-05-19 23:04:20 +0700

    Final sample

--- bin/lab.c
+++ bin/lab.c
@@ -4,8 +4,8 @@ int var1, x;
 
 char new_function(double p);
 
-int func1(int a){ 
-    char z; 
+int func1(int a){  
+    char z; 	
 } 
 int func2(int a){ 
     int x, y, z; 
@@ -21,6 +21,5 @@ int main(){
     int a, var1; 
     func1(a); 
     func2(var1); 
-    printf("%s\n", );
 }
 
