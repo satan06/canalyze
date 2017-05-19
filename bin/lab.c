59f55c33ecc76d902c4b440c284c223ef460811a
Satan Love <panmihaurkin@icloud.com>
2017-05-19 19:19:24 +0700

    Added new examples

--- bin/lab.c
+++ bin/lab.c
@@ -2,10 +2,17 @@
 
 int var1, x;
 
+char new_function(double p);
+
 int func1(int a){ 
     char z; 
 } 
 int func2(int a){ 
+    int x, y, z; 
+    float x; 
+    func1(x); 
+}
+int func3(int c){ 
     int x, y, z; 
     float x; 
     func1(x); 
@@ -14,5 +21,6 @@ int main(){
     int a, var1; 
     func1(a); 
     func2(var1); 
+    printf("%s\n", );
 }
 
