ca9b3ed9be9b4ad2a9ddf284e5c6d68c0d3b8d6e
Satan Love <panmihaurkin@icloud.com>
2017-05-20 08:32:56 +0700

    Global remaster of str search

--- src/strings.c
+++ src/strings.c
@@ -22,26 +22,15 @@ char *schr(const char *str, const char item)
 
 const char *strstr(char *hstack, char *ndle)
 {
-  char *ndle_ptr = NULL, *hstack_ptr = NULL;
-  int trigger_out = 1;
- 
-  if(hstack == NULL || ndle == NULL)
-    
-    return NULL;
- 
-  while(*hstack) {    
-    if(*hstack == *ndle) {
-      ndle_ptr = ndle;
-      hstack_ptr = hstack;
- 
-      while(*ndle_ptr && *hstack_ptr) {
-        trigger_out = trigger_out && (*ndle_ptr == *hstack_ptr);
-      }
- 
-      if(trigger_out)
-        return hstack;
+  if(hstack == NULL || ndle == NULL) {
+    return NULL;    
+  }
+
+  while(*hstack) {
+    while(hstack != ndle) {
+      ++hstack;
     }
-    ++hstack;
+    return hstack;              
   }
   return NULL;
 }
